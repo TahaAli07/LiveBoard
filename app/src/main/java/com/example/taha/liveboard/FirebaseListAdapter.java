aa1b0cc112b1b71049e9004680d0cf7cb2ba4bbc
Taha Ali <taslash74@gmail.com>
2018-03-24 21:56:38 +0530

    working model

---
+++ app/src/main/java/com/example/taha/liveboard/FirebaseListAdapter.java
@@ -0,0 +1,179 @@
+package com.example.taha.liveboard;
+
+import android.app.Activity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.Query;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public abstract class FirebaseListAdapter<T> extends BaseAdapter {
+
+    private Query mRef;
+    private Class<T> mModelClass;
+    private int mLayout;
+    private LayoutInflater mInflater;
+    private List<T> mModels;
+    private Map<String, T> mModelKeys;
+    private ChildEventListener mListener;
+
+
+    public FirebaseListAdapter(Query mRef, Class<T> mModelClass, int mLayout, Activity activity) {
+        this.mRef = mRef;
+        this.mModelClass = mModelClass;
+        this.mLayout = mLayout;
+        mInflater = activity.getLayoutInflater();
+        mModels = new ArrayList<T>();
+        mModelKeys = new HashMap<String, T>();
+        Log.v("FirebaseListAdapter", "adding child event listeners");
+        // Look for all child events. We will then map them to our own internal ArrayList, which backs ListView
+        mListener = this.mRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String previousChildName) {
+
+                T model = dataSnapshot.getValue(FirebaseListAdapter.this.mModelClass);
+                mModelKeys.put(dataSnapshot.getKey(), model);
+
+                // Insert into the correct location, based on previousChildName
+                if (previousChildName == null) {
+                    mModels.add(0, model);
+                } else {
+                    T previousModel = mModelKeys.get(previousChildName);
+                    int previousIndex = mModels.indexOf(previousModel);
+                    int nextIndex = previousIndex + 1;
+                    if (nextIndex == mModels.size()) {
+                        mModels.add(model);
+                    } else {
+                        mModels.add(nextIndex, model);
+                    }
+                }
+
+                notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                // One of the mModels changed. Replace it in our list and name mapping
+                String modelName = dataSnapshot.getKey();
+                T oldModel = mModelKeys.get(modelName);
+                T newModel = dataSnapshot.getValue(FirebaseListAdapter.this.mModelClass);
+                int index = mModels.indexOf(oldModel);
+
+                mModels.set(index, newModel);
+                mModelKeys.put(modelName, newModel);
+
+                notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                // A model was removed from the list. Remove it from our list and the name mapping
+                String modelName = dataSnapshot.getKey();
+                T oldModel = mModelKeys.get(modelName);
+                mModels.remove(oldModel);
+                mModelKeys.remove(modelName);
+                notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String previousChildName) {
+
+                // A model changed position in the list. Update our list accordingly
+                String modelName = dataSnapshot.getKey();
+                T oldModel = mModelKeys.get(modelName);
+                T newModel = dataSnapshot.getValue(FirebaseListAdapter.this.mModelClass);
+                int index = mModels.indexOf(oldModel);
+                mModels.remove(index);
+                if (previousChildName == null) {
+                    mModels.add(0, newModel);
+                } else {
+                    T previousModel = mModelKeys.get(previousChildName);
+                    int previousIndex = mModels.indexOf(previousModel);
+                    int nextIndex = previousIndex + 1;
+                    if (nextIndex == mModels.size()) {
+                        mModels.add(newModel);
+                    } else {
+                        mModels.add(nextIndex, newModel);
+                    }
+                }
+                notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                Log.e("FirebaseListAdapter", "Listen was cancelled, no more updates will occur");
+            }
+
+        });
+    }
+
+    public void cleanup() {
+
+        Log.v("FirebaseListAdapter", "removing child event listeners");
+        mRef.removeEventListener(mListener);
+        mModels.clear();
+        mModelKeys.clear();
+    }
+
+    @Override
+    public int getCount() {
+        return mModels.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return mModels.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+
+    public String getModelKey(T model) {
+        for (Map.Entry<String,T> entry: mModelKeys.entrySet()) {
+            if (entry.getValue() == model) {
+                return entry.getKey();
+            }
+        }
+        throw new IllegalArgumentException("model not found");
+    }
+
+
+    public String getModelKey(int i) {
+        Object model = getItem(i);
+        for (Map.Entry<String,T> entry: mModelKeys.entrySet()) {
+            if (entry.getValue() == model) {
+                return entry.getKey();
+            }
+        }
+        throw new IllegalArgumentException("model not found");
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        if (view == null) {
+            view = mInflater.inflate(mLayout, viewGroup, false);
+        }
+
+        T model = mModels.get(i);
+
+        populateView(view, model);
+        return view;
+    }
+
+    protected abstract void populateView(View v, T model);
+}
